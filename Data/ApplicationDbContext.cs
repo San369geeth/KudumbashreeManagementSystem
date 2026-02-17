5116aa9de2166675ae69f4d5aee5c8d4dc998e4f
sangeeth <unidentifiedm76@gmail.com>
2026-02-17 11:30:16 +0530

    Added Member model and database migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KudumbashreeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KudumbashreeManagementSystem.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Member> Members { get; set; }
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KudumbashreeManagementSystem.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Phone { get; set; }=string.Empty;
+        public string? Address { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
